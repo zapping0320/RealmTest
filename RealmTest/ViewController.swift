392fedfbc77bcc4c562152afcb172cd39c471b13
John DongHyun Kim <zapping0320@gmail.com>
2022-03-22 13:08:43 +0900

    todoDetailView 추가

--- RealmTest/ViewController.swift
+++ RealmTest/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var todoDetailView: UIView!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var statusSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        thumbImageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        thumbImageView.layer.masksToBounds = true
+        thumbImageView.contentMode = .scaleToFill
+        thumbImageView.layer.borderWidth = 1
+        
     }
 
 
